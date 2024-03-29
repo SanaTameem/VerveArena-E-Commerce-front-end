604a37f4dd753a45f635bc802b43071f528951c1
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2024-03-30 00:53:14 +0430

    Update src/App.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import LogIn from './components/LogIn';
+import ShoppingCart from './components/ShoppingCart';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/shopping-bag" element={<ShoppingCart />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
